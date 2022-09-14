586afc424b0f3397dc46def0bec40b739afde99c
Abdul qadeer <102538683+qadeer77@users.noreply.github.com>
2022-09-14 22:30:02 +0500

    updated

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const apiKey = `182aa2c63a537687d37203b245b80957`;
 const form = document.querySelector("form");
 const searchInput = document.getElementById("search-input");
 const appContainer = document.getElementById("app-container");
+const body1 = document.querySelector("#body1");
 
 const getWeather = async (city) => {
     // setTimeout(() => {
@@ -14,27 +15,39 @@ const getWeather = async (city) => {
 }
 
 const showWeather = (data) => {
-    console.log(data)
+    // console.log(data)
     if (data.cod === "404") {
         appContainer.innerHTML = `<h2 id="notFound"> City Not Found <h2>`
         return;
     }
     appContainer.innerHTML = `
     <div id="location">
-        <p>${data.name}</p>
+    <p>${data.name}</p>
     </div>
     <div id="temp">
-        <img id="temp-icon" src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" alt="">
-        <p><span class="qadeer" id="temp-value">${Math.floor(data.main.temp)}</span><span class="qadeer" id="temp-unit">&#176c</span></p>
-        <p id="climate">${data.weather[0].main}</p>
+    <img id="temp-icon" src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" alt="">
+    <p><span class="qadeer" id="temp-value">${Math.floor(data.main.temp)}</span><span class="qadeer" id="temp-unit">&#176c</span></p>
+    <p id="climate">${data.weather[0].main}</p>
     </div>
-
+    
     `
+    if (data.weather[0].main === "Smoke") {
+        body1.className += " big";
+    }
+    else if (data.weather[0].main === "Clear") {
+        body1.className += " clear";
+    }
+    else if (data.weather[0].main === "Clouds") {
+        body1.className += " clouds"
+    }
+    else if (data.weather[0].main === "Haze") {
+        body1.className += " haze"
+    }
 }
 
 
 form.addEventListener("submit", (event) => {
     getWeather(searchInput.value);
-    searchInput.value = " ";
+    // searchInput.value = " ";
     event.preventDefault();
 })
